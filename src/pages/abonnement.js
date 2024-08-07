8b4ab64936a32decb149664778e9cb3fd2457d84
3ibsi69 <a.abbassi21306@pi.tn>
2024-08-07 14:33:55 +0100

    Refactor subscription data fetching and update subscription card styles

--- src/pages/abonnement.js
+++ src/pages/abonnement.js
@@ -7,28 +7,41 @@ import PageTitle from "@/components/Reuseable/PageTitle";
 import React, { useEffect, useState } from "react";
 
 const Abonnement = () => {
-  const subscriptionData = [
-    {
-      title: "Freelancer",
-      description:
-        "All the basics for starting a new business... All the basics for starting a new business... All the basics for starting a new business...",
-      price: "24TND",
-    },
-    {
-      title: "Professional",
-      description:
-        "Advanced tools and support for your business... All the basics for starting a new business... All the basics for starting a new business...",
-      price: "49TND",
-    },
-    {
-      title: "Enterprise",
-      description:
-        "Comprehensive solutions for large businesses... All the basics for starting a new business... All the basics for starting a new business...",
-      price: "99TND",
-    },
-  ];
+  // const subscriptionData = [
+  //   {
+  //     title: "Freelancer",
+  //     description:
+  //       "All the basics for starting a new business... All the basics for starting a new business... All the basics for starting a new business...",
+  //     price: "24TND",
+  //   },
+  //   {
+  //     title: "Professional",
+  //     description:
+  //       "Advanced tools and support for your business... All the basics for starting a new business... All the basics for starting a new business...",
+  //     price: "49TND",
+  //   },
+  //   {
+  //     title: "Enterprise",
+  //     description:
+  //       "Comprehensive solutions for large businesses... All the basics for starting a new business... All the basics for starting a new business...",
+  //     price: "99TND",
+  //   },
+  // ];
+  const [subscriptionData, setSubscriptionData] = useState([]);
+  const fetchSubscriptions = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:3636/subscription/find-all"
+      );
+      const data = await response.json();
+      setSubscriptionData(data);
+    } catch (err) {
+      console.error("Error fetching subscriptions:", err);
+    }
+  };
   const [user, setUser] = useState(null);
   useEffect(() => {
+    fetchSubscriptions();
     const token = localStorage.getItem("token");
     if (token) {
       fetch("http://localhost:3636/users/verify", {
@@ -42,7 +55,6 @@ const Abonnement = () => {
         .then((data) => {
           if (data && data.firstName) {
             setUser(data);
-            console.log("User data:", data);
           } else {
             localStorage.removeItem("token");
           }
