36075c1c4e5e34ede092188bdfd7f77c8ca8f443
3ibsi69 <a.abbassi21306@pi.tn>
2024-08-20 14:10:54 +0100

    added fetch faq

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -4,14 +4,29 @@ import FaqTitleArea from "@/components/FaqArea/FaqTitleArea";
 import Header from "@/components/Header/Header";
 import Layout from "@/components/Layout/Layout";
 import PageTitle from "@/components/Reuseable/PageTitle";
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Faq = () => {
+  const [faqData, setFaqData] = useState(null);
+  const fetchFaqData = async () => {
+    try {
+      const response = await axios.get(
+        "http://194.164.54.216:3636/faq/find-all"
+      );
+      setFaqData(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.error("Error fetching faq data:", error);
+    }
+  };
+  useEffect(() => {
+    fetchFaqData();
+  }, []);
   return (
     <Layout>
       <Header />
       <PageTitle title="FAQs" parent="pages" />
-      <FaqDesignArea />
+      <FaqDesignArea faqData={faqData} />
       {/* <BrandAreaTwo /> */}
     </Layout>
   );
