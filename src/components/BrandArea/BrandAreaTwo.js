fc0cafec90102e8cdce422f490d00a1800c487cd
3ibsi69 <a.abbassi21306@pi.tn>
2024-08-29 11:58:50 +0100

    chore: Update BrandAreaTwo component to fetch and display brands from API

--- src/components/BrandArea/BrandAreaTwo.js
+++ src/components/BrandArea/BrandAreaTwo.js
@@ -1,9 +1,9 @@
 import { brands } from "@/data/brandArea";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Image } from "react-bootstrap";
 import SwiperCore, { Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import axios from "axios";
 SwiperCore.use([Autoplay]);
 
 const options = {
@@ -46,17 +46,38 @@ const BrandItem = ({ image }) => {
 };
 
 const BrandAreaTwo = ({ className = "" }) => {
+  const [brands, setBrands] = useState([]);
+  const fetchBrands = async () => {
+    try {
+      const response = await axios.get(
+        "http://localhost:3636/worked-with/find-all"
+      );
+      console.log(response.data);
+      setBrands(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchBrands();
+  }, []);
   return (
     <div className={`brand-area-2 pt-120 pb-120 ${className}`}>
       <Container>
         <div className="brand-active">
           <Swiper {...options}>
             <div className="swiper-wrapper">
-              {brands.map(({ id, image }) => (
-                <SwiperSlide key={id}>
-                  <BrandItem image={image} />
-                </SwiperSlide>
-              ))}
+              {brands.message === "No workedWidth found" && (
+                <div className="text-center w-100">
+                  <h3>No workedWidth found</h3>
+                </div>
+              )}
+              {brands.length > 0 &&
+                brands.map(({ id, image }) => (
+                  <SwiperSlide key={id}>
+                    <BrandItem image={image} />
+                  </SwiperSlide>
+                ))}
             </div>
           </Swiper>
         </div>
