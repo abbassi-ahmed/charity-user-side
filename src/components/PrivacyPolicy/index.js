c0d341b2b14b1b7b70ad3f8dfdd1824283e4758f
3ibsi69 <a.abbassi21306@pi.tn>
2024-10-31 12:36:56 +0100

    Refactor FaqDesignArea to include PrivacyPolicy component

---
+++ src/components/PrivacyPolicy/index.js
@@ -0,0 +1,173 @@
+import React from "react";
+
+export default function PrivacyPolicy() {
+  return (
+    <div className="explore-projects-item mt-30">
+      <div className="explore-projects-content" style={{ padding: "20px" }}>
+        <h2 className="title">Politique de Confidentialité</h2>
+        <p>Dernière mise à jour : 31-10-2024</p>
+        <p>
+          L’association Olympique Mnihla s’engage à protéger la vie privée de
+          ses membres et des visiteurs de son site web. Cette Politique de
+          Confidentialité explique comment nous recueillons, utilisons, et
+          protégeons les informations personnelles que vous nous fournissez lors
+          de votre visite sur notre site
+        </p>
+        <h4 className="title">1. Informations Collectées</h4>
+        <p>
+          Nous pouvons collecter les types d’informations suivants lorsque vous
+          interagissez avec notre site web :
+        </p>
+        <p>
+          <strong className="text">a. Informations Personnelles</strong>
+        </p>
+        <ul>
+          <li>
+            Données d’adhésion : Lorsque vous devenez membre de l’association,
+            nous recueillons des informations telles que votre nom, adresse
+            e-mail, numéro de téléphone, et adresse postale.
+          </li>
+          <li>
+            Données d’inscription aux événements : Lorsque vous vous inscrivez à
+            un événement, nous pouvons recueillir des informations similaires à
+            celles collectées lors de l’adhésion.
+          </li>
+        </ul>
+        <p>
+          <strong>b. Informations Automatiques</strong>
+        </p>
+        <ul>
+          <li>
+            Cookies : Nous utilisons des cookies pour améliorer votre expérience
+            sur notre site, personnaliser le contenu et analyser le trafic.{" "}
+          </li>
+          <li>
+            Données d’utilisation : Nous recueillons des informations sur votre
+            navigation, telles que votre adresse IP, le type de navigateur
+            utilisé, et les pages que vous visitez.
+          </li>
+        </ul>
+
+        <h4 className="title">2. Utilisation des Informations</h4>
+        <ul>
+          <li>
+            Gestion de l’adhésion : Pour traiter votre adhésion et gérer votre
+            compte membre.
+          </li>
+          <li>
+            Inscription aux événements : Pour organiser votre participation à
+            nos événements.
+          </li>
+          <li>
+            Communication : Pour vous envoyer des informations sur les
+            événements, activités, ou actualités de l’association.
+          </li>
+          <li>
+            Amélioration du site : Pour améliorer notre site web, notamment en
+            analysant l’utilisation du site et en ajustant notre contenu en
+            conséquence.
+          </li>
+          <li>
+            Sécurité : Pour garantir la sécurité du site et prévenir toute
+            activité frauduleuse ou non autorisée.
+          </li>
+        </ul>
+
+        <h4 className="title">3. Partage des Informations</h4>
+        <p>
+          Nous ne vendons, n’échangeons, ni ne louons vos informations
+          personnelles à des tiers. Toutefois, nous pouvons partager vos
+          informations dans les cas suivants :
+        </p>
+        <ul>
+          <li>
+            Partenaires de l’association : Avec votre accord, nous pouvons
+            partager certaines informations avec nos partenaires pour vous
+            offrir des services ou avantages liés à l’association.
+          </li>
+          <li>
+            Prestataires de services : Nous pouvons partager des informations
+            avec des prestataires de services tiers qui nous aident à gérer le
+            site web ou à organiser des événements.
+          </li>
+          <li>
+            Obligations légales : Nous pouvons divulguer vos informations si la
+            loi l’exige ou en réponse à une demande des autorités compétentes.
+          </li>
+        </ul>
+
+        <h4 className="title">4. Sécurité des Données</h4>
+        <p>
+          Nous mettons en œuvre des mesures de sécurité raisonnables pour
+          protéger vos informations personnelles contre tout accès non autorisé,
+          perte, altération ou divulgation. Cependant, aucune méthode de
+          transmission sur Internet n’est totalement sécurisée, et nous ne
+          pouvons garantir la sécurité absolue de vos données
+        </p>
+
+        <h4 className="title">5. Vos Droits</h4>
+        <ul>
+          <li>
+            Accéder à vos données personnelles : Vous pouvez demander à
+            consulter les informations que nous détenons à votre sujet.
+          </li>
+          <li>
+            Corriger vos données : Si vos informations sont incorrectes ou
+            incomplètes, vous pouvez demander leur rectification.
+          </li>
+          <li>
+            Supprimer vos données : Vous avez le droit de demander la
+            suppression de vos données personnelles, sauf si nous avons une
+            obligation légale de les conserver.
+          </li>
+          <li>
+            Retirer votre consentement : Si vous avez donné votre consentement
+            pour l’utilisation de vos données, vous pouvez le retirer à tout
+            moment.
+          </li>
+        </ul>
+        <p>Contactez-nous à [olympiquemnihla@gmail.com].</p>
+
+        <h4 className="title">6. Utilisation des Cookies</h4>
+        <p>
+          Notre site utilise des cookies pour optimiser votre expérience en
+          ligne. Vous pouvez choisir d’accepter ou de refuser les cookies via
+          les paramètres de votre navigateur. Toutefois, le refus de certains
+          cookies pourrait affecter le bon fonctionnement du site.
+        </p>
+
+        <h4 className="title">7. Liens vers des Sites Tiers</h4>
+        <p>
+          Notre site peut contenir des liens vers des sites web tiers. Nous ne
+          sommes pas responsables des pratiques de confidentialité de ces sites.
+          Nous vous encourageons à lire les politiques de confidentialité des
+          sites que vous visitez.
+        </p>
+
+        <h4 className="title">
+          8. Modifications de la Politique de Confidentialité
+        </h4>
+        <p>
+          Nous pouvons mettre à jour cette Politique de Confidentialité de temps
+          en temps. Toute modification sera publiée sur cette page, et la date
+          de mise à jour sera indiquée en haut de la page. Nous vous
+          encourageons à consulter régulièrement cette politique pour rester
+          informé.
+        </p>
+
+        <h4 className="title">9. Contact</h4>
+        <p>
+          Si vous avez des questions ou des préoccupations concernant cette
+          Politique de Confidentialité, veuillez nous contacter à :
+        </p>
+        <address>
+          Olympique Mnihla
+          <br />
+          Cite Bassatine Mnihla, Ariana, Tunisie, 2094
+          <br />
+          olympiquemnihla@gmail.com
+        </address>
+      </div>
+    </div>
+  );
+}
