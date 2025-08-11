0689bc2c47c1a48c672088b66016ee8fc7020dae
melekabbassi <abbassimelek@gmail.com>
2025-08-11 20:23:43 +0100

    fix: small changes

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -7,119 +7,55 @@ export const navItems = [
     id: 1,
     name: "Accueil",
     href: "/",
-    icon: "fa-home", // Added icons for mobile
   },
   {
     id: 2,
-    name: "À Propos",
-    href: "#", // Use # for dropdown parents
-    icon: "fa-info-circle",
+    name: "A Propos",
+    href: "#",
     subNavItems: [
-      { id: 1, name: "Olympique Mnihla", href: "/about-om", icon: "fa-shield-alt" },
-      { id: 2, name: "Équipe", href: "/about", icon: "fa-users" },
+      { id: 1, name: "Olympique Mnihla", href: "/about-om" },
+      { id: 2, name: "Équipe", href: "/about" },
     ],
   },
   {
     id: 3,
     name: "Crowdfunding",
     href: "#",
-    icon: "fa-hand-holding-usd",
     subNavItems: [
-      { id: 1, name: "SOCIAL", href: "/projects-2", icon: "fa-heart" },
-      { id: 2, name: "Startup", href: "/projects-type1", icon: "fa-lightbulb" },
-      { id: 3, name: "Projet participatif", href: "/projects-type2", icon: "fa-users-cog" },
+      { id: 1, name: "SOCIAL", href: "/projects-2" },
+      { id: 2, name: "Startup", href: "/projects-type1" },
+      { id: 3, name: "Projet participatif", href: "/projects-type2" },
     ],
   },
+
   {
     id: 4,
     name: "Nos Actions",
     href: "#",
-    icon: "fa-tasks",
     subNavItems: [
-      { id: 1, name: "Projet", href: "/project", icon: "fa-project-diagram" },
-      { id: 2, name: "Événement", href: "/events", icon: "fa-calendar-alt" },
-      { id: 3, name: "Activité", href: "/activity", icon: "fa-running" },
-      { id: 4, name: "Gallerie", href: "/gallery", icon: "fa-images" },
+      { id: 1, name: "Projet", href: "/project" },
+      { id: 2, name: "Evénement", href: "/events" },
+      { id: 3, name: "Activité", href: "/activity" },
+      { id: 4, name: "Gallerie", href: "/gallery" },
     ],
   },
   {
     id: 5,
     name: "Nos Abonnements",
     href: "#",
-    icon: "fa-id-card",
     subNavItems: [
-      { id: 1, name: "Club", href: "/abonnement-club", icon: "fa-tshirt" },
-      { id: 2, name: "Sport", href: "/abonnement-sport", icon: "fa-dumbbell" },
+      { id: 1, name: "Club", href: "/abonnement-club" },
+      { id: 2, name: "Sport", href: "/abonnement-sport" },
       {
         id: 3,
         name: "Donner abonnement",
         href: "/abonnement-form",
-        icon: "fa-gift",
         role: "moderator",
       },
     ],
   },
-  { 
-    id: 6, 
-    name: "Blog & Actualité", 
-    href: "/news",
-    icon: "fa-newspaper" 
-  },
+  { id: 6, name: "Blog & Actualité", href: "/news" },
 ];
-// export const navItems = [
-//   {
-//     id: 1,
-//     name: "Accueil",
-//     href: "/",
-//   },
-//   {
-//     id: 2,
-//     name: "A Propos",
-//     href: "#",
-//     subNavItems: [
-//       { id: 1, name: "Olympique Mnihla", href: "/about-om" },
-//       { id: 2, name: "Équipe", href: "/about" },
-//     ],
-//   },
-//   {
-//     id: 3,
-//     name: "Crowdfunding",
-//     href: "#",
-//     subNavItems: [
-//       { id: 1, name: "SOCIAL", href: "/projects-2" },
-//       { id: 2, name: "Startup", href: "/projects-type1" },
-//       { id: 3, name: "Projet participatif", href: "/projects-type2" },
-//     ],
-//   },
-
-//   {
-//     id: 4,
-//     name: "Nos Actions",
-//     href: "#",
-//     subNavItems: [
-//       { id: 1, name: "Projet", href: "/project" },
-//       { id: 2, name: "Evénement", href: "/events" },
-//       { id: 3, name: "Activité", href: "/activity" },
-//       { id: 4, name: "Gallerie", href: "/gallery" },
-//     ],
-//   },
-//   {
-//     id: 5,
-//     name: "Nos Abonnements",
-//     href: "#",
-//     subNavItems: [
-//       { id: 1, name: "Club", href: "/abonnement-club" },
-//       { id: 2, name: "Sport", href: "/abonnement-sport" },
-//       {
-//         id: 3,
-//         name: "Donner abonnement",
-//         href: "/abonnement-form",
-//         role: "moderator",
-//       },
-//     ],
-//   },
-//   { id: 6, name: "Blog & Actualité", href: "/news" },
-// ];
 export const navDefault = [
   {
     id: 1,
